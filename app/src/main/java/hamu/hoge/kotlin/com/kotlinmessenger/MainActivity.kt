2549fec6890883f7a791bc29c35d41157a660c6f
hamu <gc.kai.g@gmail.com>
2018-11-04 23:42:03 +0900

    first commit

---
+++ app/src/main/java/hamu/hoge/kotlin/com/kotlinmessenger/MainActivity.kt
@@ -0,0 +1,12 @@
+package hamu.hoge.kotlin.com.kotlinmessenger
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
